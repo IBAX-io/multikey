108e5498d9b53e4f6d3a3a7b58da96deccea9a1b
powerpook <powerkey@ibax.io>
2024-09-12 15:29:06 +0800

    Update message.ts

--- src/store/message.ts
+++ src/store/message.ts
@@ -1,21 +1,28 @@
-import { createAsyncThunk, createSlice, createEntityAdapter } from '@reduxjs/toolkit';
-import { handleMessageCount } from '@/plugins/request/api';
-import { CountParams, CountList } from '@/dataType';
+import { BalanceParams, BalanceType, CountList, CountParams } from '@/dataType';
+import { handleEcoBalance, handleMessageCount } from '@/plugins/request/api';
+import { createAsyncThunk, createEntityAdapter, createSlice } from '@reduxjs/toolkit';
 import type { RootState } from './index';
 
-interface MessageTypes extends CountList {
+interface Balance {
+  balance: BalanceType | null;
+}
+interface MessageTypes extends Balance, CountList {
   status: 'idle' | 'loading' | 'failed';
 }
-
 const messageAdapter = createEntityAdapter();
 const initialState = messageAdapter.getInitialState<MessageTypes>({
   list: [],
   count: 0,
-  status: 'idle'
+  status: 'idle',
+  balance: null
 });
 export const initMessageSearch = createAsyncThunk('message/search', async (params: CountParams) => {
   const res = (await handleMessageCount(params))!;
   return res;
+});
+export const initBalance = createAsyncThunk('message/balance', async (params: BalanceParams) => {
+  const res = (await handleEcoBalance(params))!;
+  return res[0].result;
 });
 export const messageSlice = createSlice({
   name: 'team',
@@ -34,10 +41,20 @@ export const messageSlice = createSlice({
       })
       .addCase(initMessageSearch.rejected, (state) => {
         state.status = 'failed';
+      })
+      .addCase(initBalance.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(initBalance.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.balance = action.payload ? action.payload : null;
+      })
+      .addCase(initBalance.rejected, (state) => {
+        state.status = 'failed';
       });
   }
 });
 export const count = (state: RootState) => state.message.count;
 export const loadStatus = (state: RootState) => state.message.status;
-
+export const getBalance = (state: RootState) => state.message.balance;
 export default messageSlice.reducer;
