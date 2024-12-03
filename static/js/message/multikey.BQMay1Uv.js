9b4e2d7f5c5b258fbc41efdf6f8cba4268849b05
powerpook <powerkey@ibax.io>
2024-12-03 15:17:19 +0800

    Updates

--- static/js/message/multikey.BQMay1Uv.js
+++
@@ -1,1 +0,0 @@
-import{j as e,au as a}from"../multikey/multikey.D1_AnZj2.js";const s=()=>e(a,{});s.displayName="MessagePage";export{s as Component};
---
+++ static/js/message/multikey.DM3oTGj3.js
@@ -0,0 +1,1 @@
+import{j as e,au as a}from"../multikey/multikey.BhREcByE.js";const s=()=>e(a,{});s.displayName="MessagePage";export{s as Component};
